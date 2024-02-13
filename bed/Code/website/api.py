293e0cbbedf0673d50cf78ceefa5238da4b2f811
HarshMithapara <hmithapara@horizontal.com>
2024-02-13 11:47:29 +0530

    added Library Management System Backend

--- bed/Code/website/api.py
+++ bed/Code/website/api.py
@@ -1,25 +1,32 @@
-from flask import Blueprint
-from .models import Category
+from flask import Blueprint, request, json
 from . import db
+from .models import Section
+
 
 api = Blueprint("api", __name__)
 
 
-@api.route('/getCategories', methods=['GET'])
-def getCategories():
-    categories = []
-    for category in Category.query.all():
-        item = {
-            "categoryId": category.categoryId,
-            "categoryName": category.categoryName
-        }
-        categories.append(item)
-    return categories
+@api.route("/section", methods=["GET"])
+def Sections():
+    data = Section.query.all()
+    section = []
+    for i in data:
+        item = {}
+        item["section_id"] = i.section_id
+        item["section_name"] = i.section_name
+        item["description"] = i.description
+        section.append(item)
+    return section
 
 
-@api.route('/createCategory', methods=["POST"])
-def createCategory():
-    category = Category(categoryName="test")
-    db.session.add(category)
+@api.route("/addsection", methods=["POST"])
+def AddSection():
+    data = request.get_json()
+    sections = list(Section.query.filter_by(section_name=data["section_name"]))
+    if (len(sections) > 0):
+        return "section already exists"
+    section = Section(
+        section_name=data["section_name"], description=data["description"])
+    db.session.add(section)
     db.session.commit()
-    return "Created"
+    return json.jsonify("{success: True, message: 'Section added Successfully'}")
--- bed/Code/website/models.py
+++ bed/Code/website/models.py
@@ -2,27 +2,45 @@ from website import db
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_login import UserMixin
-from sqlalchemy import DateTime, func
-from flask_security import RoleMixin
-
-
-class Category(db.Model):
-    categoryId: Mapped[int] = mapped_column(Integer, autoincrement=True,
-                                            unique=True, primary_key=True)
-    categoryName: Mapped[str] = mapped_column(String)
+from sqlalchemy import DateTime, func, ForeignKey
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = "user"
     user_id: Mapped[int] = mapped_column(
         Integer, autoincrement=True, nullable=False, primary_key=True)
     email: Mapped[str] = mapped_column(String, unique=True)
     password: Mapped[str] = mapped_column(String)
-    cart = mapped_column(String, default="[]")
-    created_on = mapped_column(
+    created_on: Mapped[DateTime] = mapped_column(
         DateTime, default=func.now())
-    roles = db.relationship("Role")
+    roles: Mapped[str] = mapped_column(String)
+    issued_books: Mapped[str] = mapped_column(String)
 
 
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
+class Section(db.Model):
+    __tablename__ = "section"
+    section_id: Mapped[int] = mapped_column(
+        Integer, autoincrement=True, nullable=False, primary_key=True)
+    section_name: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    created_on: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+
+
+class Book(db.Model):
+    __tablename__ = "book"
+    book_id: Mapped[int] = mapped_column(
+        Integer, autoincrement=True, nullable=False, primary_key=True)
+    book_name: Mapped[str] = mapped_column(String)
+    section_id: Mapped[int] = mapped_column(ForeignKey("section.section_id"))
+    content: Mapped[str] = mapped_column(String)
+    author: Mapped[str] = mapped_column(String)
+    created_on: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    issued_by: Mapped[int] = mapped_column(Integer)
+    issue_date: Mapped[str] = mapped_column(String)
+
+
+class Requests(db.Model):
+    __tablename__ = "request"
+    request_id: Mapped[int] = mapped_column(
+        Integer, autoincrement=True, nullable=False, primary_key=True)
+    request_by: Mapped[int] = mapped_column(ForeignKey("user.user_id"))
